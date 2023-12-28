36e805590d8860fed87e21b9eb936dfdeec39a9c
LaraMendesss <laraicmendes@gmail.com>
2023-12-28 12:52:37 -0500

    javascript basic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 
+
+
+
+function generateText(event){
+    event.preventDefault();
+   
+    new Typewriter('#text', {
+        strings: 'Lara Mendes',
+        autoStart: true,
+        delay: 300,
+        
+      });
+
+
+}
+
+
+
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", generateText);
+
+
+
